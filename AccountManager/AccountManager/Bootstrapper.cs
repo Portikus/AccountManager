57ec153ffba1fa2b5239d47adb7af1e3e458cbd7
Portikus <ahuisf@gmail.com>
2016-06-19 09:55:36 +0200

    Changed application startup to use Prism. Added Bootstrapper to initialite the project.

---
+++ AccountManager/AccountManager/Bootstrapper.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using AccountManager.ViewModels;
+using Microsoft.Practices.Unity;
+using Prism.Logging;
+using Prism.Unity;
+
+namespace AccountManager
+{
+    internal class Bootstrapper : UnityBootstrapper
+    {
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+        }
+
+        protected override DependencyObject CreateShell()
+        {
+            return new Shell(); 
+        }
+
+        protected override void InitializeShell()
+        {
+            var shell = ((Window)Shell);
+            shell.DataContext = Container.Resolve<ShellViewModel>();
+            Application.Current.MainWindow = shell;
+            shell.Show();
+        }
+
+        protected override ILoggerFacade CreateLogger()
+        {
+            return new DebugLogger();
+        }
+    }
+}
